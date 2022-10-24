1c1fd41ea436817bf84bfcf03f89905ca8e695e4
Joseph Ligier <joseph.ligier@accenture.com>
2022-10-24 16:15:25 +0200

    Fix image repo

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ locals {
   repo_regex = "^(?:(?P<url>[^/]+))?(?:/(?P<image>[^:]*))??(?::(?P<tag>[^:]*))"
   main_image = contains(keys(var.images), "main") ? regex(local.repo_regex, var.images.main) : {}
 
-  main_pre_value = "controller.image"
+  main_pre_value = "image"
 
-  main_set_values = local.main_image != {} ? [{ name = "${local.main_pre_value}.registry", value = local.main_image.url }, { name = "${local.main_pre_value}.image", value = local.main_image.image }, { name = "${local.main_pre_value}.tag", value = local.main_image.tag }, { name = "${local.main_pre_value}.digest", value = "false" }] : []
+  main_set_values = local.main_image != {} ? [{ name = "${local.main_pre_value}.repository", value = "${local.main_image.url}/${local.main_image.image}" }, { name = "${local.main_pre_value}.tag", value = local.main_image.tag }] : []
 
   set_values = concat(var.set_values, local.main_set_values)
 }
