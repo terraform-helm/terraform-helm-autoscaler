c4ed07a6616689cce7c53a6978708f8819309969
Joseph Ligier <joseph.ligier@accenture.com>
2023-01-03 09:52:40 +0100

    Add creation of namespace

--- main.tf
+++ main.tf
@@ -2,13 +2,12 @@ locals {
   set_values = concat(var.set_values, module.main_image.set_values)
 
   default_helm_config = {
-    name             = var.name
-    repository       = var.repository
-    chart            = var.chart
-    namespace        = var.namespace
-    create_namespace = var.create_namespace
-    version          = var.release_version
-    values           = var.values
+    name       = var.name
+    repository = var.repository
+    chart      = var.chart
+    namespace  = var.namespace
+    version    = var.release_version
+    values     = var.values
   }
 
   helm_config = merge(local.default_helm_config, var.helm_config)
@@ -22,8 +21,9 @@ module "main_image" {
 }
 
 module "helm" {
-  source               = "github.com/terraform-helm/terraform-helm?ref=0.1"
+  source               = "github.com/terraform-helm/terraform-helm?ref=main"
   helm_config          = local.helm_config
   set_values           = local.set_values
   set_sensitive_values = var.set_sensitive_values
+  create_namespace     = var.create_namespace
 }
